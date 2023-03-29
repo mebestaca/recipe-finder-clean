12cd52cf5a2707e0adf9e351d174ac3a1e7bc1b7
mebestaca <mebestaca@gmail.com>
2023-03-28 19:44:26 -0600

    added ingredients entity

---
+++ lib/feature/data/models/ingredients.dart
@@ -0,0 +1,13 @@
+class Ingredients{
+  String? name;
+  String? unitOfMeasure;
+  double? amount;
+  String? imageUrl;
+
+  Ingredients({
+    required this.name,
+    required this.amount,
+    required this.unitOfMeasure,
+    required this.imageUrl,
+  });
+}
