bccd31502ad516246b4c08f13be341deb1ccee77
mebestaca <mebestaca@gmail.com>
2023-03-28 19:51:46 -0600

    extends equatable

--- lib/feature/domain/entities/ingredients.dart
+++ lib/feature/domain/entities/ingredients.dart
@@ -1,13 +1,18 @@
-class Ingredients{
-  String? name;
-  String? unitOfMeasure;
-  double? amount;
-  String? imageUrl;
+import 'package:equatable/equatable.dart';
 
-  Ingredients({
+class Ingredients extends Equatable{
+  final String name;
+  final String unitOfMeasure;
+  final double amount;
+  final String imageUrl;
+
+  const Ingredients({
     required this.name,
     required this.amount,
     required this.unitOfMeasure,
     required this.imageUrl,
   });
+
+  @override
+  List<Object?> get props => [name, unitOfMeasure, amount, imageUrl];
 }
--- lib/feature/domain/entities/recipe.dart
+++ lib/feature/domain/entities/recipe.dart
@@ -1,7 +1,18 @@
+import 'package:equatable/equatable.dart';
+
 import 'ingredients.dart';
 
-class Recipe{
-  String? title;
-  String? imageUrl;
-  List<Ingredients>? ingredients;
+class Recipe extends Equatable{
+  final String title;
+  final String imageUrl;
+  final List<Ingredients> ingredients;
+
+  const Recipe({
+    required this.title,
+    required this.imageUrl,
+    required this.ingredients,
+  });
+
+  @override
+  List<Object?> get props => [ingredients, title, imageUrl];
 }
