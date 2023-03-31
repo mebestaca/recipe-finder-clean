f71834e462ce6908e89c20d0840f9838ff4124f5
mebestaca <mebestaca@gmail.com>
2023-03-30 20:01:20 -0600

    added ingredients model

---
+++ lib/feature/data/models/ingredients_model.dart
@@ -0,0 +1,20 @@
+import '../../domain/entities/ingredients.dart';
+
+class IngredientsModel extends Ingredients{
+  const IngredientsModel({
+    required super.name,
+    required super.amount,
+    required super.unitOfMeasure,
+    required super.imageUrl
+  });
+
+  factory IngredientsModel.fromJson(Map<String, dynamic> json) {
+    return IngredientsModel(
+      name: json["name"] ?? "",
+      amount: json["amount"]?.toDouble() ?? 0,
+      unitOfMeasure: json["originalName"] ?? "",
+      imageUrl: json["image"] ?? "",
+    );
+  }
+
+}
