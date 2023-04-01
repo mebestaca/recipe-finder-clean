29aea64832fa4fca57d12d9d6e1680e8b1218a01
mebestaca <mebestaca@gmail.com>
2023-03-31 18:04:42 -0600

    added recipe repository implementation

--- lib/feature/data/repositories/recipe_repository_impl.dart
+++ lib/feature/data/repositories/recipe_repository_impl.dart
@@ -1,9 +1,24 @@
 import 'package:dartz/dartz.dart';
 import 'package:recipe_finder_clean/core/error/failure.dart';
+import 'package:recipe_finder_clean/feature/data/datasources/recipe_local_data_source.dart';
+import 'package:recipe_finder_clean/feature/data/datasources/recipe_remote_data_source.dart';
 import 'package:recipe_finder_clean/feature/domain/entities/recipe.dart';
 import 'package:recipe_finder_clean/feature/domain/repositories/recipe_repository.dart';
 
+import '../../../core/network/network_info.dart';
+
 class RecipeRepositoryImpl extends RecipeRepository{
+
+  final RecipeRemoteDataSource remoteDataSource;
+  final RecipeLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  RecipeRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo
+  });
+
   @override
   Future<Either<Failure, List<Recipe>>> getRecipe(List<String> ingredients) async {
     return const Right(
