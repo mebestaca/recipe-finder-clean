400da5662af7b3870fbf82b6095b7b3ff439cf24
mebestaca <mebestaca@gmail.com>
2023-04-02 18:17:33 -0600

    changed remote data source contract return type from recipe to list of recipe

--- lib/feature/data/datasources/recipe_remote_data_source.dart
+++ lib/feature/data/datasources/recipe_remote_data_source.dart
@@ -4,5 +4,5 @@ import '../../../core/error/failure.dart';
 import '../../domain/entities/recipe.dart';
 
 abstract class RecipeRemoteDataSource{
-  Future<Either<Failure, Recipe>> getRecipe(List<String> ingredients);
+  Future<Either<Failure, List<Recipe>>> getRecipe(List<String> ingredients);
 }
