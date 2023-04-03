c3b1bc062f6540c6ebe84782bdb5507aea72f9a7
mebestaca <mebestaca@gmail.com>
2023-04-02 18:19:08 -0600

    test shoudl return remote data when the call to the remote is successful

--- test/feature/data/repositories/recipe_repository_impl_test.dart
+++ test/feature/data/repositories/recipe_repository_impl_test.dart
@@ -1,10 +1,13 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:recipe_finder_clean/core/network/network_info.dart';
 import 'package:recipe_finder_clean/feature/data/datasources/recipe_local_data_source.dart';
 import 'package:recipe_finder_clean/feature/data/datasources/recipe_remote_data_source.dart';
+import 'package:recipe_finder_clean/feature/data/models/recipe_model.dart';
 import 'package:recipe_finder_clean/feature/data/repositories/recipe_repository_impl.dart';
+import 'package:recipe_finder_clean/feature/domain/entities/ingredients.dart';
 
 import 'recipe_repository_impl_test.mocks.dart';
 
@@ -24,22 +27,53 @@ void main() {
   mockRecipeLocalDataSource = MockRecipeLocalDataSource();
   mockNetworkInfo = MockNetworkInfo();
 
+
+  
   repository = RecipeRepositoryImpl(
       localDataSource: mockRecipeLocalDataSource,
       remoteDataSource: mockRecipeRemoteDataSource,
       networkInfo: mockNetworkInfo
   );
 
-  test(
-    "should check if the device is online",
-      (){
-        when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+  group(
+    "getRecipe",
+      () {
+        List<String> ingredients = ["salt", "pepper"];
+        const tRecipeModel = RecipeModel(
+            title: "test'",
+            imageUrl: "test url",
+            ingredients: [Ingredients(
+                name: "test name",
+                amount: 1,
+                unitOfMeasure: "cup",
+                imageUrl: "image url")]);
 
-        repository.getRecipe(["salt", "pepper"]);
+        test(
+          "should check if the device is online",
+              () {
+            when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+            repository.getRecipe(ingredients);
+            verify(mockNetworkInfo.isConnected);
+          }
+        );
 
-        verify(mockNetworkInfo.isConnected);
+        group("device is online",
+            () {
+              setUp(() {
+                when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+              });
+              test("should return remote data when the call to remote data source is successful",
+                      () async {
+                    when(mockRecipeRemoteDataSource.getRecipe(ingredients))
+                        .thenAnswer((_) async => const Right([tRecipeModel]));
+                    final result = await repository.getRecipe(ingredients);
+                    verify(mockRecipeRemoteDataSource.getRecipe(ingredients));
+                    expect(result, equals(const Right([tRecipeModel])));
+                  }
+              );
+            }
+        );
       }
   );
-
 }
 
