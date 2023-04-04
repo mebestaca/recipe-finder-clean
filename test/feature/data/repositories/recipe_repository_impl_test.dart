cbe85fe1a94c047b45cb37e4826bfeaa72181a85
mebestaca <mebestaca@gmail.com>
2023-04-04 12:17:53 -0600

    corrected test parameters on "should return remote data when the call to remote data source is successful" test

--- test/feature/data/repositories/recipe_repository_impl_test.dart
+++ test/feature/data/repositories/recipe_repository_impl_test.dart
@@ -1,6 +1,9 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:recipe_finder_clean/core/error/exception.dart';
+import 'package:recipe_finder_clean/core/error/failure.dart';
 import 'package:recipe_finder_clean/core/network/network_info.dart';
 import 'package:recipe_finder_clean/feature/data/datasources/recipe_local_data_source.dart';
 import 'package:recipe_finder_clean/feature/data/datasources/recipe_remote_data_source.dart';
@@ -58,32 +61,43 @@ void main() {
         );
 
         group("device is online",
-            () {
-              setUp(() {
-                when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
-              });
+          () {
+            setUp(() {
+              when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+            });
 
-              test("should return remote data when the call to remote data source is successful",
-                      () async {
-                    when(mockRecipeRemoteDataSource.getRecipe(ingredients))
-                        .thenAnswer((_) async => tRecipeModel);
-                    final result = await repository.getRecipe(ingredients);
-                    final resultFold = result.fold((l) => null, (r) => r);
-                    verify(mockRecipeRemoteDataSource.getRecipe(ingredients));
-                    expect(resultFold, equals(tRecipe));
-                  }
-              );
+            test("should return remote data when the call to remote data source is successful",
+              () async {
+                when(mockRecipeRemoteDataSource.getRecipe(ingredients))
+                    .thenAnswer((_) async => tRecipeModel);
+                final result = await repository.getRecipe(ingredients);
+                verify(mockRecipeRemoteDataSource.getRecipe(ingredients));
+                expect(result, equals(Right(tRecipe)));
+              }
+            );
 
-              test("should cache the data locally when the call to remote data source is successful",
-                  () async {
-                    when(mockRecipeRemoteDataSource.getRecipe(ingredients))
-                        .thenAnswer((_) async => tRecipeModel);
-                    await repository.getRecipe(ingredients);
-                    verify(mockRecipeRemoteDataSource.getRecipe(ingredients));
-                    verify(mockRecipeLocalDataSource.cacheRecipeList(tRecipeModel));
-                  }
-              );
-            }
+            test("should cache the data locally when the call to remote data source is successful",
+              () async {
+                when(mockRecipeRemoteDataSource.getRecipe(ingredients))
+                    .thenAnswer((_) async => tRecipeModel);
+                await repository.getRecipe(ingredients);
+                verify(mockRecipeRemoteDataSource.getRecipe(ingredients));
+                verify(mockRecipeLocalDataSource.cacheRecipeList(tRecipeModel));
+              }
+            );
+            /*
+            test("should return server failure when the call to the remote data source is unsuccessful",
+              () async {
+                when(mockRecipeRemoteDataSource.getRecipe(ingredients))
+                    .thenThrow(ServerException());
+                final result = await repository.getRecipe(ingredients);
+                verify(mockRecipeRemoteDataSource.getRecipe(ingredients));
+                verifyZeroInteractions(mockRecipeLocalDataSource);
+                expect(result, equals(Left(ServerFailure())));
+              }
+            );
+            */
+          }
         );
       }
   );
