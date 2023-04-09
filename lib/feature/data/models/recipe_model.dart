b1158c1378ec43428b211924a12240024c709990
mebestaca <mebestaca@gmail.com>
2023-04-09 15:05:33 -0600

    added list of ingredients to model

--- lib/feature/data/models/recipe_model.dart
+++ lib/feature/data/models/recipe_model.dart
@@ -3,10 +3,13 @@ import 'package:recipe_finder_clean/feature/domain/entities/recipe.dart';
 import 'ingredients_model.dart';
 
 class RecipeModel extends Recipe{
+
+  final List<IngredientsModel> ingredients;
+
   const RecipeModel({
     required super.title,
     required super.imageUrl,
-    required super.ingredients
+    required this.ingredients,
   });
 
   factory RecipeModel.fromJson(Map<String, dynamic> json) {
@@ -18,4 +21,10 @@ class RecipeModel extends Recipe{
         ),
     );
   }
+
+  Map<String, dynamic> toJson() => {
+    "title" : title,
+    "image" : imageUrl,
+    "missedIngredients" : List<dynamic>.from(ingredients.map((e) => e.toJson()))
+  };
 }
