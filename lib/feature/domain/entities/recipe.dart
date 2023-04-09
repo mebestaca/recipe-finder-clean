7591cb475c061841308c06585581e0b01576011b
mebestaca <mebestaca@gmail.com>
2023-04-09 15:05:17 -0600

    removed list of ingredients as it violates the clean architecture

--- lib/feature/domain/entities/recipe.dart
+++ lib/feature/domain/entities/recipe.dart
@@ -1,18 +1,14 @@
 import 'package:equatable/equatable.dart';
 
-import 'ingredients.dart';
-
 class Recipe extends Equatable{
   final String title;
   final String imageUrl;
-  final List<Ingredients> ingredients;
 
   const Recipe({
     required this.title,
     required this.imageUrl,
-    required this.ingredients,
   });
 
   @override
-  List<Object?> get props => [ingredients, title, imageUrl];
+  List<Object?> get props => [title, imageUrl];
 }
