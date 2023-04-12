735dfd8e71b1f884d376ddff9f5cdb82cd941ff9
mebestaca <mebestaca@gmail.com>
2023-04-12 02:29:38 -0600

    implemented recipe local data source. added getLastRecipeList

--- lib/feature/data/datasources/recipe_local_data_source.dart
+++ lib/feature/data/datasources/recipe_local_data_source.dart
@@ -1,7 +1,37 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../models/recipe_model.dart';
 
 abstract class RecipeLocalDataSource{
   Future<List<RecipeModel>> getLastRecipeList();
 
   Future<void> cacheRecipeList(List<RecipeModel> recipeList);
+}
+
+const cachedRecipeListKey = "CACHED_RECIPE_LIST";
+class RecipeLocalDataSourceImpl implements RecipeLocalDataSource{
+
+  final SharedPreferences sharedPreferences;
+
+  RecipeLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<void> cacheRecipeList(List<RecipeModel> recipeList) async {
+
+  }
+
+  @override
+  Future<List<RecipeModel>> getLastRecipeList() async {
+    final jsonString = sharedPreferences.getString(cachedRecipeListKey);
+    final List dataList = jsonDecode(jsonString!);
+
+    return Future.value(
+      List<RecipeModel>.from(dataList.map((e) {
+        return RecipeModel.fromJson(e);
+      }))
+    );
+  }
+
 }
