799bbef7614941fde9c894481269837279bc01af
mebestaca <mebestaca@gmail.com>
2023-04-12 03:40:32 -0600

    added cacheRecipeList test

--- test/feature/data/datasources/recipe_local_data_source_test.dart
+++ test/feature/data/datasources/recipe_local_data_source_test.dart
@@ -5,6 +5,7 @@ import "package:mockito/annotations.dart";
 import "package:mockito/mockito.dart";
 import "package:recipe_finder_clean/core/error/exception.dart";
 import "package:recipe_finder_clean/feature/data/datasources/recipe_local_data_source.dart";
+import "package:recipe_finder_clean/feature/data/models/ingredients_model.dart";
 import "package:recipe_finder_clean/feature/data/models/recipe_model.dart";
 import "package:shared_preferences/shared_preferences.dart";
 
@@ -30,11 +31,9 @@ void main() {
     test("should return List<RecipeModel> from SharedPreferences when there is one in the cache", () async {
       when(mockSharedPreferences.getString(any))
         .thenReturn(fixture("recipe_cached.json"));
-
       final result = await dataSource.getLastRecipeList();
       verify(mockSharedPreferences.getString(cachedRecipeListKey));
       expect(result, equals(tRecipeListModel));
-
     });
 
     test("should throw a CacheException when there is not a cache value", () async {
@@ -42,7 +41,24 @@ void main() {
       final call = dataSource.getLastRecipeList;
       expect(() => call(), throwsA(const TypeMatcher<CacheException>()));
     });
+  });
 
+  group("cacheRecipeList", () {
+    final List<RecipeModel> tRecipeList = [ const RecipeModel(
+        title: "test'",
+        imageUrl: "test url",
+        ingredients: [IngredientsModel(
+            name: "test name",
+            amount: 1,
+            unitOfMeasure: "cup",
+            imageUrl: "image url")])];
+
+    test("should call SharedPreferences to cache the data", () {
+      dataSource.cacheRecipeList(tRecipeList);
+      List<Map<String, dynamic>> dataList = tRecipeList.map((e) => e.toJson()).toList();
+      final String expectedJsonString = jsonEncode(dataList);
+      verify(mockSharedPreferences.setString(cachedRecipeListKey, expectedJsonString));
+    });
   });
 
 }
