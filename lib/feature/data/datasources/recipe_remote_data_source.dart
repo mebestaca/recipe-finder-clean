9fc5ed77317f682d283678b14694a8112503ea51
mebestaca <mebestaca@gmail.com>
2023-04-13 15:12:55 -0600

    implemented recipe remote data source

--- lib/feature/data/datasources/recipe_remote_data_source.dart
+++ lib/feature/data/datasources/recipe_remote_data_source.dart
@@ -1,5 +1,26 @@
+import '../../../keys/keys.dart';
 import '../models/recipe_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class RecipeRemoteDataSource{
   Future<List<RecipeModel>> getRecipe(List<String> ingredients);
+}
+
+class RecipeRemoteDataSourceImpl implements RecipeRemoteDataSource{
+
+  final http.Client client;
+
+  const RecipeRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<List<RecipeModel>> getRecipe(List<String> ingredients) async {
+    Map<String, String> data= {
+      "ingredients" : ingredients.toString(),
+      "apiKey" : apiKey,
+      "number" : "100"
+    };
+    final uri = Uri.http('api.spoonacular.com', '/recipes/findByIngredients', data);
+    final response = await client.get(uri, headers: {"Content-Type" : "application/json"});
+    return Future.value([]);
+  }
 }
