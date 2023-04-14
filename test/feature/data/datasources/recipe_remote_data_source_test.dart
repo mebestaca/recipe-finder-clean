31eb1a425d8c5bbfe2bff6eb5f0e58bef70d94be
mebestaca <mebestaca@gmail.com>
2023-04-13 20:53:17 -0600

    added test that checks if the function returns the correct list of models and error handling

--- test/feature/data/datasources/recipe_remote_data_source_test.dart
+++ test/feature/data/datasources/recipe_remote_data_source_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
+import 'package:recipe_finder_clean/core/error/exception.dart';
 import 'package:recipe_finder_clean/feature/data/datasources/recipe_remote_data_source.dart';
+import 'package:recipe_finder_clean/feature/data/models/ingredients_model.dart';
+import 'package:recipe_finder_clean/feature/data/models/recipe_model.dart';
 import 'package:recipe_finder_clean/keys/keys.dart';
 import 'package:http/http.dart' as http;
 
@@ -30,11 +32,35 @@ void main() {
 
     test("should perform a GET request on a URL with with a list being the endpoint and with application/json header", () {
       when(mockClient.get(any, headers: anyNamed("headers")))
-        .thenAnswer((_) async => Response(fixture("recipe.json"), 200));
+        .thenAnswer((_) async => http.Response(fixture("recipe.json"), 200));
       final uri = Uri.http('api.spoonacular.com', '/recipes/findByIngredients', data);
       dataSource.getRecipe(ingredients);
 
       verify(mockClient.get(uri, headers: {"Content-Type" : "application/json"}));
+    });
+
+
+    final List<RecipeModel> tRecipeList = [ const RecipeModel(
+        title: "test title",
+        imageUrl: "image url",
+        ingredients: [IngredientsModel(
+            name: "salt",
+            amount: 1.0,
+            unitOfMeasure: "cup",
+            imageUrl: "test.url.com")])];
+    test("should return a list of recipe model when the response code is 200 (success)", () async {
+      when(mockClient.get(any, headers: anyNamed("headers")))
+        .thenAnswer((_) async => http.Response(fixture("recipe.json"), 200));
+      final result = await dataSource.getRecipe(ingredients);
+      expect(result, equals(tRecipeList));
+    });
+
+    test("should throw a ServerException when the response code   is 404 or other", () async {
+      when(mockClient.get(any, headers: anyNamed("headers")))
+        .thenAnswer((_) async => http.Response("something went wrong", 404));
+
+      final call = dataSource.getRecipe;
+      expect(() => call(ingredients), throwsA(const TypeMatcher<ServerException>()));
     });
   });
 }
