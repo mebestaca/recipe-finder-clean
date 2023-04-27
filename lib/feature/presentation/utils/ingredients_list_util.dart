af288e115fe8345a7d537f95c8c82e24685065aa
mebestaca <mebestaca@gmail.com>
2023-04-27 09:57:16 -0600

    added ingredients list utility class for keeping a list of ingredients in the presentation layer

---
+++ lib/feature/presentation/utils/ingredients_list_util.dart
@@ -0,0 +1,24 @@
+abstract class IngredientsListProvider{
+  void addIngredient(String ingredient);
+  void removeIngredient(String ingredient);
+  List<String> getIngredientList();
+}
+
+class IngredientsList implements IngredientsListProvider{
+  List<String> ingredientsList = [];
+
+  @override
+  void addIngredient(String ingredient) {
+    ingredientsList.add(ingredient);
+  }
+
+  @override
+  void removeIngredient(String ingredient) {
+    ingredientsList.remove(ingredient);
+  }
+
+  @override
+  List<String> getIngredientList() {
+    return ingredientsList;
+  }
+}
