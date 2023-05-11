d59d0aaed7cf739a22674709939285d7b76ea001
mebestaca <mebestaca@gmail.com>
2023-05-10 22:17:48 -0600

    added recipe list loading page

---
+++ lib/feature/presentation/widgets/recipe_list_loading.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecipeListLoading extends StatelessWidget {
+  const RecipeListLoading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 3,
+      child: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
