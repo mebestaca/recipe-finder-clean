c83aebecb9ca690a43572db2aea1959102c7f45f
mebestaca <mebestaca@gmail.com>
2023-05-10 22:17:21 -0600

    added recipe list loaded page

---
+++ lib/feature/presentation/widgets/recipe_list_loaded.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recipe_finder_clean/feature/presentation/bloc/recipe/recipe_list_bloc.dart';
+
+import '../../../injection_container.dart';
+
+class RecipeListLoaded extends StatelessWidget {
+  const RecipeListLoaded({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
