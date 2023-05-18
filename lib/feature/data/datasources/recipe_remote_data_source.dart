58e6798ca0eed3960ac91d4fe27bd7042894bdce
mebestaca <mebestaca@gmail.com>
2023-05-17 23:24:19 -0600

    now uses https intead of http when requesting for data

--- lib/feature/data/datasources/recipe_remote_data_source.dart
+++ lib/feature/data/datasources/recipe_remote_data_source.dart
@@ -22,8 +22,10 @@ class RecipeRemoteDataSourceImpl implements RecipeRemoteDataSource{
       "apiKey" : apiKey,
       "number" : "100"
     };
-    final uri = Uri.http('api.spoonacular.com', '/recipes/findByIngredients', data);
+    final uri = Uri.https('api.spoonacular.com', '/recipes/findByIngredients', data);
+    print(uri);
     final response = await client.get(uri, headers: {"Content-Type" : "application/json"});
+    print(response.statusCode);
 
     if (response.statusCode == 200) {
       final List dataList = jsonDecode(response.body);
