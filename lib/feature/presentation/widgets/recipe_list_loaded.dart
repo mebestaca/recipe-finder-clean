01bbdebeb4d68852a17a831a57b1f52d43a5ea5f
mebestaca <mebestaca@gmail.com>
2023-05-19 17:16:53 -0600

    havent decided how to display the list

--- lib/feature/presentation/widgets/recipe_list_loaded.dart
+++ lib/feature/presentation/widgets/recipe_list_loaded.dart
@@ -3,12 +3,23 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recipe_finder_clean/feature/presentation/bloc/recipe/recipe_list_bloc.dart';
 
 import '../../../injection_container.dart';
+import '../../domain/entities/recipe.dart';
 
 class RecipeListLoaded extends StatelessWidget {
   const RecipeListLoaded({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<Recipe> recipeList = ModalRoute.of(context)?.settings.arguments as List<Recipe>;
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Container();
+          }
+      ),
+    );
   }
 }
