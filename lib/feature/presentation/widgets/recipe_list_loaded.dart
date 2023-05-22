7746410dfedfff0f43de9c60e9d93d21ceceb017
mebestaca <mebestaca@gmail.com>
2023-05-22 15:51:21 -0600

    added a list to display the retrieved data

--- lib/feature/presentation/widgets/recipe_list_loaded.dart
+++ lib/feature/presentation/widgets/recipe_list_loaded.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recipe_finder_clean/feature/presentation/bloc/recipe/recipe_list_bloc.dart';
-
-import '../../../injection_container.dart';
 import '../../domain/entities/recipe.dart';
 
 class RecipeListLoaded extends StatelessWidget {
-  const RecipeListLoaded({Key? key, required this.recipeList}) : super(key: key);
+  const RecipeListLoaded({Key? key, required this.recipeList, required this.recipeListBloc}) : super(key: key);
+  final RecipeListBloc recipeListBloc;
   final List<Recipe> recipeList;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Placeholder(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            recipeListBloc.add(ReturnToMenu());
+          },
+        ),
+
+      ),
+      body: ListView.builder(
+          itemCount: recipeList.length,
+          itemBuilder: (context, index) {
+              return Card(
+                child: Text(recipeList[index].title),
+              );
+          }
+      ),
     );
   }
 }
