2fe369942eed9f8934fcc0647e0956696ca7f58b
mebestaca <mebestaca@gmail.com>
2023-05-23 15:25:01 -0600

    partially implemented recipe view

--- lib/feature/presentation/widgets/states/recipe_list_view.dart
+++ lib/feature/presentation/widgets/states/recipe_list_view.dart
@@ -1,15 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_finder_clean/feature/presentation/bloc/recipe/recipe_list_bloc.dart';
 
-import '../../../domain/entities/recipe.dart';
+import '../../../data/models/recipe_model.dart';
 
 class RecipeView extends StatelessWidget {
   const RecipeView({Key? key, required this.recipe, required this.recipeListBloc}) : super(key: key);
-  final Recipe recipe;
+  final RecipeModel recipe;
   final RecipeListBloc recipeListBloc;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe.title),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                    child: Center(
+                        child: Text(recipe.title)
+                    )
+                )
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.network(
+                      recipe.imageUrl,
+                      fit: BoxFit.fill,
+                      loadingBuilder: (_, child, loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                loadingProgress.expectedTotalBytes!
+                                : null,
+                          ),
+                        );
+                      }
+                    ),
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: recipe.ingredients.isNotEmpty,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Card(
+                    child: ExpansionTile(
+                      title: const Text("Used Ingredients"),
+                      children: [
+                        ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemCount: recipe.ingredients.length,
+                            itemBuilder: (context, index) {
+                              return const Placeholder();
+                              // return SharedListTile(recipe: recipe.usedIngredients[index]);
+                            }
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
