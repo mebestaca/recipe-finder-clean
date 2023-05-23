f88a469679bb033d1186bf380931fb283417a42a
mebestaca <mebestaca@gmail.com>
2023-05-22 22:15:53 -0600

    implemented an onTap event

--- lib/feature/presentation/widgets/recipe_list/recipe_list_card.dart
+++ lib/feature/presentation/widgets/recipe_list/recipe_list_card.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
 
 import '../../../domain/entities/recipe.dart';
+import '../../bloc/recipe/recipe_list_bloc.dart';
 
 class RecipeListCard extends StatelessWidget {
-  const RecipeListCard({Key? key, required this.recipe}) : super(key: key);
+  const RecipeListCard({Key? key, required this.recipe, required this.recipeListBloc}) : super(key: key);
   final Recipe recipe;
+  final RecipeListBloc recipeListBloc;
 
   @override
   Widget build(BuildContext context) {
       return Card(
         child: ListTile(
-            title: Image.network(
-              recipe.imageUrl,
-              fit: BoxFit.fill,
-              loadingBuilder: ( _, child, loadingProgress) {
-                if (loadingProgress == null) return child;
-                return Center(
-                  child: CircularProgressIndicator(
-                    value: loadingProgress.expectedTotalBytes != null
-                        ? loadingProgress.cumulativeBytesLoaded /
-                        loadingProgress.expectedTotalBytes!
-                        : null,
-                  ),
-                );
+            title: GestureDetector(
+              onTap: () {
+                recipeListBloc.add(OnTapRecipe(recipe));
               },
+              child: Image.network(
+                recipe.imageUrl,
+                fit: BoxFit.fill,
+                loadingBuilder: ( _, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  );
+                },
+              ),
             ),
-            subtitle: Center(child: Text(recipe.title))),
+            subtitle: Text(recipe.title,
+              textScaleFactor: 1.2,
+              textAlign: TextAlign.center,)),
       );
     }
 }
