1bfa3ef4051cf646f3373db31c666899baa20613
mebestaca <mebestaca@gmail.com>
2023-05-24 18:01:33 -0600

    added original attribute

--- lib/feature/domain/entities/ingredients.dart
+++ lib/feature/domain/entities/ingredients.dart
@@ -5,14 +5,16 @@ class Ingredients extends Equatable{
   final String unitOfMeasure;
   final double amount;
   final String imageUrl;
+  final String original;
 
   const Ingredients({
     required this.name,
     required this.amount,
     required this.unitOfMeasure,
     required this.imageUrl,
+    required this.original,
   });
 
   @override
-  List<Object?> get props => [name, unitOfMeasure, amount, imageUrl];
+  List<Object?> get props => [name, unitOfMeasure, amount, imageUrl, original];
 }
