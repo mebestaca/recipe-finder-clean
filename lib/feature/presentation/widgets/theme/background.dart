5df9da89cafe104a9f35408f872fe0d7c7ee343f
mebestaca <mebestaca@gmail.com>
2023-05-24 18:01:21 -0600

    added background

---
+++ lib/feature/presentation/widgets/theme/background.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/food_background.jpg"),
+            fit: BoxFit.cover
+        ),
+      ),
+    );
+  }
+}
