7575d262c83c55a260b75ab6cced258f38c80b62
mebestaca <mebestaca@gmail.com>
2023-05-24 18:04:30 -0600

    displaying food details now uses navigation instead of bloc

--- lib/feature/presentation/widgets/recipe_list/recipe_list_card.dart
+++ lib/feature/presentation/widgets/recipe_list/recipe_list_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../data/models/recipe_model.dart';
 import '../../bloc/recipe/recipe_list_bloc.dart';
+import '../states/recipe_list_view.dart';
 
 class RecipeListCard extends StatelessWidget {
   const RecipeListCard({Key? key, required this.recipe, required this.recipeListBloc}) : super(key: key);
@@ -14,22 +15,29 @@ class RecipeListCard extends StatelessWidget {
         child: ListTile(
             title: GestureDetector(
               onTap: () {
-                recipeListBloc.add(OnTapRecipe(recipe));
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => RecipeView(recipe: recipe,),
+                  ),
+                );
               },
-              child: Image.network(
-                recipe.imageUrl,
-                fit: BoxFit.fill,
-                loadingBuilder: ( _, child, loadingProgress) {
-                  if (loadingProgress == null) return child;
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                          loadingProgress.expectedTotalBytes!
-                          : null,
-                    ),
-                  );
-                },
+              child: Hero(
+                tag: recipe.id,
+                child: Image.network(
+                  recipe.imageUrl,
+                  fit: BoxFit.fill,
+                  loadingBuilder: ( _, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                            loadingProgress.expectedTotalBytes!
+                            : null,
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
             subtitle: Text(recipe.title,
