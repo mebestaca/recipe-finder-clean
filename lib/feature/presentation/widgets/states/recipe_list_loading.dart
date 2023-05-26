307775cba7fe376829a676a3d9daeb3e5b46fa01
mebestaca <mebestaca@gmail.com>
2023-05-25 20:49:59 -0600

    improved ui

--- lib/feature/presentation/widgets/states/recipe_list_loading.dart
+++ lib/feature/presentation/widgets/states/recipe_list_loading.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../theme/background.dart';
+
 class RecipeListLoading extends StatelessWidget {
   const RecipeListLoading({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height / 3,
-      child: const Center(
-        child: CircularProgressIndicator(),
+    return const SafeArea(
+      child: Stack(
+        children: [
+          Background(),
+          Center(
+            child: SizedBox(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
       ),
     );
   }
