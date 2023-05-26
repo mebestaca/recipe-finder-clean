28ea1bc2e73b9c732ea9c7542aa101b13b8631d3
mebestaca <mebestaca@gmail.com>
2023-05-25 20:49:52 -0600

    improved ui

--- lib/feature/presentation/widgets/recipe_list/recipe_list_card.dart
+++ lib/feature/presentation/widgets/recipe_list/recipe_list_card.dart
@@ -11,38 +11,41 @@ class RecipeListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      return Card(
-        child: ListTile(
-            title: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => RecipeView(recipe: recipe,),
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Card(
+          child: ListTile(
+              title: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => RecipeView(recipe: recipe,),
+                    ),
+                  );
+                },
+                child: Hero(
+                  tag: recipe.id,
+                  child: Image.network(
+                    recipe.imageUrl,
+                    fit: BoxFit.fill,
+                    loadingBuilder: ( _, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-              child: Hero(
-                tag: recipe.id,
-                child: Image.network(
-                  recipe.imageUrl,
-                  fit: BoxFit.fill,
-                  loadingBuilder: ( _, child, loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Center(
-                      child: CircularProgressIndicator(
-                        value: loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                            loadingProgress.expectedTotalBytes!
-                            : null,
-                      ),
-                    );
-                  },
                 ),
               ),
-            ),
-            subtitle: Text(recipe.title,
-              textScaleFactor: 1.2,
-              textAlign: TextAlign.center,)),
+              subtitle: Text(recipe.title,
+                textScaleFactor: 1.2,
+                textAlign: TextAlign.center,)),
+        ),
       );
     }
 }
